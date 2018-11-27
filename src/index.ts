3897d85421ad00aef01599fe18043f348f9cae55
Oleg Gerasimenko <oleg.gerasimenko@nhs.net>
2018-11-27 14:40:52 +0000

    Added 7_2 to export + changed version

--- src/index.ts
+++ src/index.ts
@@ -3,10 +3,12 @@
  */
 
 import * as build_v7_1 from "./build_7_1";
+import * as build_v7_2 from "./build_7_2";
 
 /**
  * Another documentation
  */
 export {
-    build_v7_1
+    build_v7_1,
+    build_v7_2
 }
